681b68887a0ecd265b4fe5b75e5e5029f202f2ff
Luis Custodio <luis.custodio@gmail.com>
2014-09-27 19:28:12 -0400

    storing the content on mongo

--- controllers/dataStorage.js
+++ controllers/dataStorage.js
@@ -3,11 +3,14 @@
 var mongoose = require('mongoose');
 var backTrackLocationModel = require('../models/backTrackLocationModel');
 
-module.exports.aggregate = function (data) {
+module.exports.aggregate = function (rawData) {
   
-  ///todo: data validation
+  var data = JSON.parse(rawData);
 
-  var model = new backTrackLocationModel(data);
+  var model = new backTrackLocationModel({
+    time: data.time, 
+    accels: data.accels
+   });
 
   model.save(function (err, model) {
     if (err) return console.error(err);
--- models/backTrackLocationModel.js
+++ models/backTrackLocationModel.js
@@ -2,11 +2,8 @@
 
 var mongoose = require("mongoose");
 var backTrackLocationSchema = mongoose.Schema({
-	timestamp: Date,
-  	sensor1: {x: Number, y: Number, z: Number},
-  	sensor2: {x: Number, y: Number, z: Number},
-  	sensor3: {x: Number, y: Number, z: Number},
-  	sensor4: {x: Number, y: Number, z: Number}
+	time: 	Date,
+  	accels: [{x: Number, y: Number, z: Number}]
 });
 
 // instanceSchema.statics.customFunctionName = function() {
