fde246f78461dd0d19eb9bededff10cd0f2443cb
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-27 10:35:12 -0400

    Added the home partial, removed unnecessary code

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 /**
  * Module dependencies.
  */
-
+//---------------------------------------------
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
+
 // all environments
+//---------------------------------------------
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,13 +23,18 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
+//---------------------------------------------
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+// routes
+//---------------------------------------------
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/:id', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
