d3a9843641c4197e406f10c3c4051aefd42366b6
Luis Custodio <luis.custodio@gmail.com>
2014-09-27 14:08:38 -0400

    adding new files that will talk with mongo

---
+++ controllers/dataAnalysis.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//module called dataanalysis
+
+module.exports.aggregate = function (param) {
+  console.log(param);
+}
--- controllers/socket.js
+++ controllers/socket.js
@@ -1,18 +1,29 @@
 "use strict";
 
+var dataAnalysis = require('./dataAnalysis');
+dataAnalysis.aggregate("data is coming");
+
 module.exports = function (io) {
   io.on('connection', function (socket) {
+    
+    console.log('something connected ' + socket);
+    
     socket.on('message', function (from, msg) {
- 
-      console.log('recieved message from', from, 'msg', JSON.stringify(msg));
- 
+      
+      console.log('...recieved message from', from, 'msg', JSON.stringify(msg));
       console.log('broadcasting message');
       console.log('payload is', msg);
+
       io.sockets.emit('broadcast', {
         payload: msg,
         source: from
       });
+
+
+      
       console.log('broadcast complete');
     });
+
+    socket.on('disconnect', function() { console.puts("Client has disconnected"); }) ;
   });
 };
