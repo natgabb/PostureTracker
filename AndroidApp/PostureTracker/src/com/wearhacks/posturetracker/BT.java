2c9a6d08372e63fb1f0211cd17b338cadded1acb
natachag <natacha.gabbamonte@gmail.com>
2014-09-27 16:13:18 -0400

    Android App now used btclient demo and now sends POST request to server

---
+++ AndroidApp/PostureTracker/src/com/wearhacks/posturetracker/BT.java
@@ -0,0 +1,7 @@
+package com.wearhacks.posturetracker;
+
+public class BT {
+	public static final String INTENT_FILTER = "com.octeight.api.accel";
+	public static final String EXTRA_TIME = "com.octeight.api.extra.time";
+	public static final String EXTRA_DATA = "com.octeight.api.extra.data";
+}
--- AndroidApp/PostureTracker/src/com/wearhacks/posturetracker/MainActivity.java
+++ AndroidApp/PostureTracker/src/com/wearhacks/posturetracker/MainActivity.java
@@ -1,84 +1,137 @@
 package com.wearhacks.posturetracker;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private Socket socket;
-    public static final int SERVERPORT = 3000;
-    private static final String SERVER_IP = "192.168.1.41";
+	public static final int SERVERPORT = 3000;
+	private static final String SERVER_IP = "192.168.1.41";
+	
+	private HashMap<String,String> gRequestBodyValue = new HashMap<String,String>();
+	
+	private final BroadcastReceiver dataReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.v("PostureTracker",
+					"At beginning of onReceive for BroadcastReceiver!");
+			double[] accels = intent.getDoubleArrayExtra(BT.EXTRA_DATA);
+			long time = intent.getLongExtra(BT.EXTRA_TIME, 0);
+			TextView dataText = (TextView) findViewById(R.id.text_data);
+			dataText.setText("" + time + "\n" + Arrays.toString(accels));
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new Thread(new ClientThread()).start();
-    }
+			// Setting request data
+			String accelsJson = "[";
+			for (int i = 0; i < accels.length; i++) {
+				accelsJson += accels[i];
+				if (i > 0)
+					accelsJson += ",";
+			}
+			accelsJson += "]";
+			gRequestBodyValue.put("event", "message");
+			gRequestBodyValue.put("source", "PostureTrackerApp");
+			gRequestBodyValue.put("payload", "{\"time\": \""+ time + "\",\"accels\":" + accelsJson + "}");
+			
+			// Doing http request in separate thread
+			// Sending to server
+			new Thread(new Runnable(){
+				@Override
+				public void run() {
+					try {
+						HttpClient client = new DefaultHttpClient();
+						HttpPost post = new HttpPost(
+								"http://"+SERVER_IP+":"+SERVERPORT+"/api/android");
+						List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+								1);
+						for(String s: gRequestBodyValue.keySet())
+							nameValuePairs.add(new BasicNameValuePair(s,gRequestBodyValue.get(s)));
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void onClick(View view){
-    	try{
-    		Log.i("PostureTracker", "Clicked Button!");
-    		EditText et = (EditText) findViewById(R.id.EditText01);
-    		String str = et.getText().toString();
-    		str = "[\"broadcast\",{\"payload\":\""+str+"\",\"source\":\"PostureTrackerApp\"}]";
-    		
-    		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-    		out.println(str);
-    		
-    		// Displaying Toast
-    		Context context = getApplicationContext();
-    		CharSequence text = "Sent message " + str + " to server!";
-    		int duration = Toast.LENGTH_SHORT;
-    		Toast toast = Toast.makeText(context, text, duration);
-    		toast.show();
-    		
-    		Log.i("PostureTracker", text.toString());
-    		et.setText("");
-    		
-    	}catch(UnknownHostException e){
-    		e.printStackTrace();
-    	}catch(IOException e){
-    		e.printStackTrace();
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
-    }
-    
-    class ClientThread implements Runnable{
-    	@Override
-    	public void run(){
-    		try{
-    			InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-    			socket = new Socket(serverAddr, SERVERPORT);
-    		}catch(UnknownHostException e1){
-    			e1.printStackTrace();
-    		}catch(IOException e2){
-    			e2.printStackTrace();
-    		}
-    	}
-    }
+						post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+						HttpResponse response = client.execute(post);
+						BufferedReader rd = new BufferedReader(new InputStreamReader(
+								response.getEntity().getContent()));
+						String line = "";
+						while ((line = rd.readLine()) != null) {
+							System.out.println(line);
+						}
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}					
+				}
+			}).start();
+			// end sending to server
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		IntentFilter filter = new IntentFilter(BT.INTENT_FILTER);
+		registerReceiver(dataReceiver, filter);
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		unregisterReceiver(dataReceiver);
+		super.onPause();
+	}
+
+	public static class PlaceholderFragment extends Fragment {
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
 }
